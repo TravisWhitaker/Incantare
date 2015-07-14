160784955ebd8996df921e9046f022b0c2a18324
Travis Whitaker <pi.boy.travis@gmail.com>
2015-07-14 19:55:27 -0400

    Added English phonemes.

---
+++ src/Language/Incantare/English/Phonemes.hs
@@ -0,0 +1,109 @@
+{-|
+Module      : Language.Incantare.English.Phonemes
+Description : Syllabic Manipulation Library
+Copyright   : Travis Whitaker 2015
+License     : MIT
+Maintainer  : pi.boy.travis@gmail.com
+Stability   : Provisional
+Portability : POSIX
+
+Approximate list of English language phonemes. Import qualified if you'd like to
+use phonemes from multiple languages.
+-}
+
+module Language.Incantare.English.Phonemes where
+
+import Data.List (intercalate)
+
+data Phon = Aa -- odd
+          | Ae -- at
+          | Ah -- hut
+          | Ao -- ought
+          | Aw -- cow
+          | Ax -- abaft
+          | Ay -- hide
+          | Ea -- wear
+          | Eh -- Ed
+          | Er -- hurt
+          | Ey -- as in "ate"
+          | Ia -- as in "fortieth"
+          | Ih -- as in "it"
+          | Iy -- as in "teen"
+          | Oh -- as in "mob"
+          | Ow -- as in "lobe"
+          | Oy -- as in "toy"
+          | Ua -- as in "intellectual"
+          | Uh -- as in "nook"
+          | Uw -- as in "two"
+          | P  -- as in "pick"
+          | B  -- as in "be"
+          | T  -- as in "tip"
+          | D  -- as in "dee"
+          | F  -- as in "fee"
+          | V  -- as in "vise"
+          | Th -- as in "thick"
+          | Dh -- as in "thee" (runic "eth")
+          | S  -- as in "sick"
+          | Z  -- as in "zip"
+          | Sh -- as in "ship"
+          | Zh -- as in "seizure"
+          | Ch -- as in "cheese"
+          | Jh -- as in "jeep"
+          | K  -- as in "key"
+          | Ng -- as in "rang"
+          | G  -- as in "green"
+          | M  -- as in "me"
+          | N  -- as in "new"
+          | L  -- as in "lee"
+          | R  -- as in "ream"
+          | W  -- as in "win"
+          | Y  -- as in "you"
+          | Hh -- as in "he"
+          deriving (Eq, Ord)
+
+instance Show Phon where
+    show Aa = "aa"
+    show Ae = "ae"
+    show Ah = "ah"
+    show Ao = "ao"
+    show Aw = "aw"
+    show Ax = "ax"
+    show Ay = "ay"
+    show Ea = "ea"
+    show Eh = "eh"
+    show Er = "er"
+    show Ey = "ey"
+    show Ia = "ia"
+    show Ih = "ih"
+    show Iy = "iy"
+    show Oh = "oh"
+    show Ow = "ow"
+    show Oy = "oy"
+    show Ua = "ua"
+    show Uh = "uh"
+    show Uw = "uw"
+    show P  = "p"
+    show B  = "b"
+    show T  = "t"
+    show D  = "d"
+    show F  = "f"
+    show V  = "v"
+    show Th = "th"
+    show Dh = "dh"
+    show S  = "s"
+    show Z  = "z"
+    show Sh = "sh"
+    show Zh = "zh"
+    show Ch = "ch"
+    show Jh = "jh"
+    show K  = "k"
+    show Ng = "ng"
+    show G  = "g"
+    show M  = "m"
+    show N  = "n"
+    show L  = "l"
+    show R  = "r"
+    show W  = "w"
+    show Y  = "y"
+    show Hh = "hh"
+    showList = (++) . intercalate "-" . map show
